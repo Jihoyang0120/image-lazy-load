aecc5d343b5eb29c87e79989e46c3ed0d6fc57ea
Jihoyang0120 <jyangca@connect.ust.hk>
2022-03-22 16:14:32 +0900

    initial commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const _path = path.join(__dirname, "./dist");
+const _path = path.join(__dirname, "./");
 app.use("/", express.static(_path));
 app.listen(8888, () => {
   console.log("lazy Image server: 8888시작 http://127.0.0.1:8888");
